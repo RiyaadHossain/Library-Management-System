8c20e16f1fad9dca405d23b41741fc5f67605adf
RiyaadHossain <riyadhossain.dev@gmail.com>
2024-12-02 20:01:55 +0600

    data seed: attempt3

--- books/apps.py
+++ books/apps.py
@@ -5,6 +5,6 @@ class BooksConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'books'
 
-    def ready(self):
-        from .load_data_on_startup import load_data_if_needed
-        load_data_if_needed()
+    # def ready(self):
+    #     from .load_data_on_startup import load_data_if_needed
+    #     load_data_if_needed()
--- books/load_data_on_startup.py
+++ books/load_data_on_startup.py
@@ -3,6 +3,6 @@ from django.db import connection
 
 def load_data_if_needed():
     with connection.cursor() as cursor:
-        cursor.execute("SELECT COUNT(*) FROM your_app_name_yourmodelname;")
+        cursor.execute("SELECT COUNT(*) FROM books_Book;")
         if cursor.fetchone()[0] == 0:  # Replace with a check for your use case
             call_command("loaddata", "data.json")
