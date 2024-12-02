7a68afbb5f0d0658f049a84a6d1b48dc0f9b0e95
RiyaadHossain <riyadhossain.dev@gmail.com>
2024-12-02 15:43:10 +0600

    data seed

--- books/apps.py
+++ books/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class BooksConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'books'
+
+    def ready(self):
+        from .load_data_on_startup import load_data_if_needed
+        load_data_if_needed()
