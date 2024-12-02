aa92ccbea63103d76a886877e190b7ba40af8b44
RiyaadHossain <riyadhossain.dev@gmail.com>
2024-12-02 20:05:21 +0600

    data seed: attempt4

--- books/load_data_on_startup.py
+++ books/load_data_on_startup.py
@@ -1,8 +1,1 @@
-from django.core.management import call_command
-from django.db import connection
 
-def load_data_if_needed():
-    with connection.cursor() as cursor:
-        cursor.execute("SELECT COUNT(*) FROM books_Book;")
-        if cursor.fetchone()[0] == 0:  # Replace with a check for your use case
-            call_command("loaddata", "data.json")
